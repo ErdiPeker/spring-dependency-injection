253d0d9abef5a95031d2f7bea92eba86f1a857ed
Erdi Peker <erdipeker@gmail.com>
2023-01-06 13:34:06 +0300

    set up the primary and profile annotations inside java configuration class

--- src/main/java/com/erdipeker/pets/DogPetService.java
+++ src/main/java/com/erdipeker/pets/DogPetService.java
@@ -1,8 +1,11 @@
 package com.erdipeker.pets;
 
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
+
 @Service
+@Profile("dog")
 public class DogPetService implements PetService {
     @Override
     public String getPetType() {
--- src/main/java/com/erdipeker/springdependencyinjection/config/GreetingServiceConfig.java
+++ src/main/java/com/erdipeker/springdependencyinjection/config/GreetingServiceConfig.java
@@ -1,10 +1,10 @@
 package com.erdipeker.springdependencyinjection.config;
 
-import com.erdipeker.springdependencyinjection.services.ConstructorGreetingService;
-import com.erdipeker.springdependencyinjection.services.PropertyInjectedGreetingService;
-import com.erdipeker.springdependencyinjection.services.SetterInjectedGreetingService;
+import com.erdipeker.springdependencyinjection.services.*;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
 
 @Configuration
 public class GreetingServiceConfig {
@@ -23,4 +23,16 @@ public class GreetingServiceConfig {
     SetterInjectedGreetingService setterInjectedGreetingService(){
         return new SetterInjectedGreetingService();
     }
+
+    @Bean
+    @Primary
+    PrimaryGreetingService primaryGreetingService(){ return new PrimaryGreetingService();}
+
+    @Profile("EN")
+    @Bean("i18nService")
+    I18nEnglishGreetingService i18nEnglishGreetingService(){ return new I18nEnglishGreetingService();}
+
+    @Profile({"ES","default"})
+    @Bean("i18nService")
+    I18nSpanishGreetingService i18nSpanishGreetingService(){ return new I18nSpanishGreetingService();}
 }
